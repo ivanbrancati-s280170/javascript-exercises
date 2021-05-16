2039a98d503f86e3cffeedca7dd84b9bc0168c6e
ivanbrancati-s280170 <s280170@studenti.polito.it>
2021-05-16 11:03:33 +0200

    Modified lecture 'flpper' example

---
+++ wa1-week10/flip-client/src/Flipper.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react' ;
+function Flipper(props) {
+
+    const [text, setText] = useState('') ;
+    const [flipped, setFlipped] = useState('') ;
+
+    //aggiungiamo uno use effect che dipende dallo state text
+    useEffect(() =>{
+        async function loadFlipped(){
+            const response = await fetch('/api/flip?text='+text) ;
+            const flippedText = await response.json() ;
+            setFlipped(flippedText.text) ;
+        } ;
+        loadFlipped() ;
+    }, [text]) ;
+    return <div>
+        Text: <input type='text'value={text} onChange={(ev) => setText(ev.target.value)}></input><br/>
+        Flipped: {flipped}
+        </div>
+}
+
+export default Flipper ;
